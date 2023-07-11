7e69dad2acbbaf479106828bd9e10f8204f4598c
Rasyid <rasshit.dsgn@gmail.com>
2023-07-11 16:02:23 +0700

    23/6. Source code for this Lecture.mp4

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,24 +4,48 @@
      @csrf
 
      <div class="form-group">
-        <label>Name</label>
-        <input type="text" name="name" value="{{ old('name') }}" required class="form-control">
-     </div>
+      <label>Name</label>
+      <input name="name" value="{{ old('name') }}" required 
+        class="form-control{{ $errors->has('name') ? ' is-invalid': '' }}">
 
-     <div class="form-group">
+        {{-- this is for error mesage --}}
+        @if ($errors->has('name'))
+        <span class="invalid-feedback">
+            <strong>{{ $errors->first('name') }}</strong>
+        </span>
+        @endif
+    </div>
+    
+    <div class="form-group">
         <label>E-mail</label>
-        <input type="text" name="email" value="{{ old('email') }}" required class="form-control">
-     </div>
-
-     <div class="form-group">
+        <input name="email" value="{{ old('email') }}" required 
+        class="form-control{{ $errors->has('email') ? ' is-invalid': '' }}">
+        
+        {{-- this is for error mesage --}}
+        @if ($errors->has('email'))
+        <span class="invalid-feedback">
+            <strong>{{ $errors->first('email') }}</strong>
+        </span> 
+        @endif
+    </div>
+    
+    <div class="form-group">
         <label>Password</label>
-        <input type="text" name="password" required class="form-control">
-     </div>
-
-     <div class="form-group">
-        <label>Retyped Password</label>
-        <input type="text" name="password_confirmation" required class="form-control">
-     </div>
+        <input name="password" required type="password"
+        class="form-control{{ $errors->has('password') ? ' is-invalid': '' }}">
+        
+        {{-- this is for error mesage --}}
+        @if ($errors->has('password'))
+          <span class="invalid-feedback">
+            <strong>{{ $errors->first('password') }}</strong>
+          </span> 
+        @endif
+    </div>
+  
+    <div class="form-group">
+    <label>Retyped Password</label>
+    <input name="password_confirmation" required class="form-control" type="password">
+    </div>  
 
      <button type="submit" class="btn btn-primary btn-block">Register</button>
     </form>
