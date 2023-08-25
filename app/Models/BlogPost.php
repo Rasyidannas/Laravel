73b43182f6283159abba234310f39f22b758028e
rasyid annas <rasshit.dsgn@gmail.com>
2023-08-26 05:55:17 +0700

    30/5.Querying the ManyToMany realtion and Pivot table

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -31,7 +31,7 @@ class BlogPost extends Model
 
     public function tags()
     {
-        return $this->belongsToMany('App\Models\Tag');
+        return $this->belongsToMany('App\Models\Tag')->withTimestamps();
     }
 
     //this is local scope
