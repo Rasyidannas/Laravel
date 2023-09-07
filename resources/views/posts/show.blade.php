06aa66d716e6ca515b76ea578372dd095868d44a
rasyid annas <rasshit.dsgn@gmail.com>
2023-09-08 04:51:16 +0700

    32/8. Pratical Displaying uploaded image and styling.mp4

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,13 +7,28 @@
 @section('content')
 <div class="row">  
     <div class="col-8">
-        <h1>
+        @if ($post->image)
+            <div style="background-image: url('{{ $post->image->url() }}'); min-height: 500px; color: white; text-align:center; background-attachment: fixed;">
+                {{-- <img src="{{ Storage::url($post->image->path) }}"/> --}}
+                {{-- this is form Image Model using function url() --}}
+                {{-- <img src="{{ $post->image->url() }}"/> --}}
+                <h1 style="padding-top: 100px; text-shadow: 1px 2px #000">
+        @else
+            <h1>
+        @endif
             {{ $post->title }}
             {{-- This is usig component --}}
             <x-badge :show="now()->diffInMinutes($post->created_at) < 5">
                 Brand new post!
             </x-badge>
-        </h1>
+
+        @if ($post->image)
+                </h1>
+            </div>
+        @else
+                </h1>
+        @endif
+
         <p>{{ $post->content }}</p>
         {{-- this is using component  --}}
         <x-updated :date="$post->created_at" :name="$post->user->name" />
@@ -22,10 +37,6 @@
         <x-tags :tags="$post->tags" />
 
         <p>Currently read by {{ $counter }} people</p>
-
-        {{-- <img src="{{ Storage::url($post->image->path) }}"/> --}}
-        {{-- this is form Image Model using function url() --}}
-        <img src="{{ $post->image->url() }}"/>
 
         <h1>Comments</h1>
 
