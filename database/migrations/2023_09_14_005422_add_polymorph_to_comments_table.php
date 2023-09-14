19a6324a643eef4bb4a4a4a647260a138b77a317
rasyid annas <rasshit.dsgn@gmail.com>
2023-09-14 14:03:32 +0700

    34/6. OneToMany Polymorphic seeder.mp4

--- database/migrations/2023_09_14_005422_add_polymorph_to_comments_table.php
+++ database/migrations/2023_09_14_005422_add_polymorph_to_comments_table.php
@@ -27,7 +27,7 @@ return new class extends Migration
         Schema::table('comments', function (Blueprint $table) {
             $table->dropMorphs('commentable');
 
-            $table->unsignedInteger('blog_post_id')->index();
+            $table->unsignedInteger('blog_post_id')->index()->nullable();
             $table->foreign('blog_post_id')->references('id')->on('blog_posts');
         });
     }
