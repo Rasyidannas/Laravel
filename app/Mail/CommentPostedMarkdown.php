302f004486cd6d1c16b1bc55e6e290265f5c10fa
rasyid annas <rasshit.dsgn@gmail.com>
2023-09-23 09:21:45 +0700

    37/3. Optional e-mail queuing and execution delay.mp4

--- app/Mail/CommentPostedMarkdown.php
+++ app/Mail/CommentPostedMarkdown.php
@@ -10,7 +10,7 @@ use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 use App\Models\Comment;
 
-class CommentPostedMarkdown extends Mailable implements ShouldQueue
+class CommentPostedMarkdown extends Mailable
 {
     use Queueable, SerializesModels;
 
