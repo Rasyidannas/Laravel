ad745d64d10df2473be7d40b7eca67ba22f714b2
rasyid annas <rasshit.dsgn@gmail.com>
2023-09-29 14:55:41 +0700

    39/3. Translating plural forms, passing data.mp4

---
+++ lang/es/messages.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'welcome' => '¡Bienvenido a Laravel!',
+    'example_with_value' => 'Ola! :name',
+    'plural' => '{0} Sin comentarios aún :a|{1} :count comentario :a|[2] :count commentarios :a'
+];
