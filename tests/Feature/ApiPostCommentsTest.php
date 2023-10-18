94541874f331b3ad29da061e3c2c92e6fff087eb
rasyid annas <rasshit.dsgn@gmail.com>
2023-10-18 16:45:52 +0700

    44/2. Testing API GET methods, veryfing JSON.mp4

--- tests/Feature/ApiPostCommentsTest.php
+++ tests/Feature/ApiPostCommentsTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\BlogPost;
+use App\Models\Comment;
 use Faker\Factory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -24,7 +25,41 @@ class ApiPostCommentsTest extends TestCase
         //this for API test
         $response = $this->getJson('/api/v1/posts/1/comments');
         $response->assertStatus(200)
-            ->assertJsonStructure(['data'])//this for check sturcture JSON has 'data'
-            ->assertJsonCount(0, 'data');//for check "data is empty comment
+            ->assertJsonStructure(['data']) //this for check sturcture JSON has 'data'
+            ->assertJsonCount(0, 'data'); //for check "data is empty comment
+    }
+
+    public function testBlogPostHas10Comments()
+    {
+        $post = new BlogPost();
+        $comment = new Comment();
+
+        //create post with 10 comments
+        $post::factory()->create([
+            'user_id' => $this->user()->id //this is from TestCase.php(parent this class)
+        ])->each(function (BlogPost $post) use ($comment) {
+            $post->comments()->saveMany(
+                $comment::factory()->count(10)->make([
+                    'user_id' => $this->user()->id
+                ])
+            );
+        });
+
+        //this for API test
+        $response = $this->getJson('/api/v1/posts/2/comments');
+        $response->assertStatus(200)
+            ->assertJsonStructure(['data' => [
+                '*' => [
+                    'comment_id', 
+                    'content', 
+                    'created_at', 
+                    'updated_at', 
+                    'user' => [
+                        'id', 
+                        'name'
+                        ]
+                    ]
+                ]]) //this for check sturcture JSON has 'data'
+            ->assertJsonCount(10, 'data'); //for check "data is empty comment
     }
 }
